2690d69d71207f0fb185b86d37054c33199d3437
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-26 19:44:27 -0600

    activ16.cpp

---
+++ activ16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define PI  3.1416
+int main() 
+{
+	float perimetro, r;
+	
+	std::cout<< "Intruduzca el radio de la circunferencia: "<< std::endl;
+	std::cin>> r;
+	
+	perimetro = 2 * PI * r;
+	std::cot<< "El perimetro de la circunferencia es: " << perimetro << std::endl;
+	
+	return 0;
+	
+}
+
