c8695fc6ee93a41faeeccf31a57becdaf8177ad1
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-26 19:23:31 -0600

    activ13.5.cpp

---
+++ activ13.5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+	int a,b,c, suma;
+	std::cout << "introduzca 5,7,9 respectivamente" << std::endl;
+	std::cin>> a;
+	std::cin>> b;
+	std::cin>> c;
+	
+		suma=a+b+c;
+		std::cout<< "La suma de 5+9+7 es igual a : " << suma << std::endl;
+}
