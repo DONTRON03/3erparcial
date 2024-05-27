74183f6e07ffedd73d398552d2fd98cb2f679605
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-26 19:19:42 -0600

    Add files via upload

---
+++ Actividad2.1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int a,b,c, suma;
+	printf("introduzca 5,7,9 respectivamente");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	
+		suma=5+9+7;
+		printf("La suma de 5+9+7 es igual a : %d",suma);
+}
+
