bc0a5c4ba47563920fbff64a2fedc565ddecfb40
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-26 19:42:06 -0600

    activ15.cpp

---
+++ activ15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+	int b,a,area;
+	std::cout<< "Introduce base y altura del triangulo rectangulo: " << std::endl;
+	std::cin>> b;
+	std::cin>> a;
+	
+	//procesoxddd
+	area = (b * a)/2;
+	std::cout<< " Un triangulo rectangulo tiene una altura de " << a << "y base de " << b << "tiene un area de: " << area << std::endl;
+	
+}
