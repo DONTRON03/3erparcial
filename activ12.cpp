b7100031d9ac116274d96a238b3b470eba658e06
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-26 18:43:44 -0600

    activ12.cpp

---
+++ activ12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+	
+	int fecha, cumple;
+	std::cout<< "Hola mundo, otra vez " << std::endl;
+	std::cout<< "Por favor introduzca el año en que nacio"<< std::endl;
+	std::cin>> fecha;
+	cumple= 2024 - fecha;
+	
+	std::cout<< "Si usted nacio en " << fecha << std::endl;
+	stad::cout << "Enconces cumples %i " << cumple << std::endl;
+
+	return 0;
+}
