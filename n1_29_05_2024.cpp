cfc7d364e36502f4c688847b81c335d2f559aa2a
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-29 12:53:38 -0600

    actividades 49 y 50

---
+++ n1_29_05_2024.cpp
@@ -0,0 +1,36 @@
+// Actividad1_vectores 29/05/24
+#include <iostream>
+#include <vector>
+
+int main(void){
+	
+	const int numsmax = 20;
+	std::vector <int> numero;
+	int num, suma = 0;
+	
+	std::cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+	std::cout << "  Favor de introducir solo numeros enteros " << std::endl;
+	std::cout << "( 0 para terminar o agrege hasta 20 numeros ) " << std::endl;
+	std::cout << "  " << std::endl;
+	
+	for (int i = 0; i < numsmax ; ++i) {
+		std::cout << "Numero " << i +1 << " : " << std::endl;
+		std::cin >> num;
+		
+		if (num == 0){
+			break;
+		}
+		
+		numero.push_back(num);
+	}
+	std::cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+	std::cout << "  los numeros en orden inverso van: " << std::endl;
+	
+	for(int i = numero.size()-1 ; i >=0; i--) {
+		std::cout << numero[i] << std::endl;
+		suma += numero[i];
+	}
+		std::cout << " la suma de todos los numeros da: " << suma << std::endl;
+	
+	std::cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+}
---
+++ n2_29_05_2024.cpp
@@ -0,0 +1,44 @@
+//Actividad2_FilasyColumnas 29/05/24
+#include <iostream>
+int main() {
+    const int fila = 4;
+    const int columna = 3;
+    int tabla[fila][columna];
+
+	std::cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+	std::cout << "   " << std::endl;
+    std::cout << "Introduce los valores para la tabla:" << std::endl;
+    std::cout << "   " << std::endl;
+    
+    
+    for (int i = 0; i < fila; ++i) {
+        for (int j = 0; j < columna; ++j) {
+            std::cout << "Valor en fila " << i + 1 << ", columna " << j + 1 << ": " << std::endl;
+            std::cin >> tabla[i][j];
+        }
+    }
+
+
+    int suma_columnas[columna] = {0};
+    for (int j = 0; j < columna; ++j) {
+        for (int i = 0; i < fila; ++i) {
+            suma_columnas[j] += tabla[i][j];
+        }
+    }
+
+
+    int mayor_suma = suma_columnas[0];
+    for (int j = 1; j < columna; ++j) {
+        if (suma_columnas[j] > mayor_suma) {
+            mayor_suma = suma_columnas[j];
+        }
+    }
+    
+	std::cout << "   " << std::endl;
+	std::cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+	std::cout << "   " << std::endl;
+    std::cout << "La suma mayor de las columnas es: " << mayor_suma << std::endl;
+}
+
+
+
