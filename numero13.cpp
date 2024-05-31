9a45ca7b2323b25f67a8fc65baa14dcb399fc9f0
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-05-30 21:02:57 -0600

    SE AGREGAN MATRICES A LA ACTIVIDAD 13

--- numero13.cpp
+++ numero13.cpp
@@ -1,36 +1,60 @@
 // Actividad 13_ 27 de mayo del 2024 _ 3er parcial
+//Modificacion con matrices
 #include <iostream>
 
 int main() {
 	
-    int acum = 0;
+    int sumf = 0;
+    int sumf2 = =;
     int f;
-    std::cout << "Digite el número de filas: ";
+    std::cout << "Digite el nÃºmero de filas: ";
     std::cin >> f;
-
     int c;
-    std::cout << "Digite el número de columnas: ";
+    std::cout << "Digite el nÃºmero de columnas: ";
     std::cin >> c;
+    int A[f][c],B[f][c],C[f][c];
 
-    int matriz[f][c]; 
+    //INGRESO DE DATOS Y SUMA DE ELEMENTOS
+    std::cout<<"Matriz A:  \n";
     for (int i = 0; i < f; ++i) {
         for (int j = 0; j < c; ++j) {
             int numero;
             std::cout << "Digite dato para la fila " << i << " columna " << j << ": ";
-            std::cin >> numero;
-            matriz[i][j] = numero;
-            acum += matriz[i][j];
+            std::cin >> A[i][j];
+            acum += A[i][j];
         }
     }
-    
-    std::cout << "Los datos que hay en la matriz son:" << std::endl;
+	std::cout << "La suma de todos los elementos de la columna es: " << sumf << std::endl;
+	std::cout<< "\n ";
+
+    std::cout<<"Matriz B:  \n";
     for (int i = 0; i < f; ++i) {
         for (int j = 0; j < c; ++j) {
-            std::cout << matriz[i][j] << " ";
+            int numero;
+            std::cout << "Digite dato para la fila " << i << " columna " << j << ": ";
+            std::cin >> B[i][j];
+            acum += B[i][j];
+        }
+    }
+	std::cout << "La suma de todos los elementos de la columna es: " << sumf << std::endl;
+	std::cout<< "\n ";
+
+    //DATOS DE LAS MATRICES
+    std::cout << "Los datos que hay en la matriz  A son:" << std::endl;
+    for (int i = 0; i < f; ++i) {
+        for (int j = 0; j < c; ++j) {
+            std::cout << A[i][j] << " ";
         }
         std::cout << std::endl;
     }
 
-    std::cout << "La suma de todos los elementos de la columna es: " << acum << std::endl;
+	    std::cout << "Los datos que hay en la matriz B son:" << std::endl;
+    for (int i = 0; i < f; ++i) {
+        for (int j = 0; j < c; ++j) {
+            std::cout << B[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
+
 
