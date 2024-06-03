2dbb035c783c0f9c56d43880c458a9fb306d5c7f
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-03 17:19:12 -0600

    abreviación de std y c++

--- numerosunicosydetergentes.cpp
+++ numerosunicosydetergentes.cpp
@@ -1,18 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 int main(){
 	int cm1,cm2;
-	printf("escriba 2 numeros:");
-	scanf("%d",&cm1);
-	scanf("%d",&cm2);
+        cout << "Escriba 2 numeros:";
+	cin >> cm1;
+	cin >> cm2;
 	
 	if(cm1>cm2){
-		printf("%d es mayor",cm1);
+		cout << cm1 << " es mayor" << endl;
 	} else if (cm1<cm2){
-		printf("%d es mayor",cm2);
+		cout << cm2 << " es mayor" << endl;
 	}
 	
 	if(cm1==cm2){
-		printf("Ambos son iguales");
+		cout << "Ambos son iguales" << endl;
 	}
 	
 	return 0;
