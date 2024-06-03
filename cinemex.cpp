c71f4fac129320ae5f95a4140ffb23140d006011
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-03 17:15:57 -0600

    abreviación de std y c++

--- cinemex.cpp
+++ cinemex.cpp
@@ -1,22 +1,24 @@
-#include <stdio.h>
+#include <iostream>
 #define precio 70
+using namespace std;
+
 int main(){
 	float edad,mayor,menor,descuento,descuento1;
-	printf ("El precio original es de $70, ingrese su edad para saber su descuento:");
-	scanf("%f",&edad);
+	cout << "El precio original es de $70, ingrese su edad para saber su descuento:";
+        cin >> edad;
 	  
 	if (edad<5){
 		menor=0.6*precio;
 		descuento=precio-menor;
-		printf("su descuento es del 60, en total es: %f",descuento);
+		 cout << "Su descuento es del 60%, en total es: " << descuento << endl;
 		
 	}else if(edad>60){
 		mayor=0.55*precio;
 		descuento1=precio-mayor;
-		printf("Su descuento es del 55, en total es: %f",descuento1);
+		cout << "Su descuento es del 55%, en total es: " << descuento1 << endl;
 	} else if	< (edad>4){
-		printf("No hay descuento, debe pagar 70 pesos");
+		std::cout << "No hay descuento, debe pagar 70 pesos" << std::endl;
 	}else if (edad<61){
-		printf("Su edad no amerita descuento.  Cantidad a pagar:70 pesos");
+		cout << "Su edad no amerita descuento.  Cantidad a pagar: 70 pesos" << endl;
 	}
 }
