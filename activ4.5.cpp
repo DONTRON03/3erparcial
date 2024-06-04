07f554a339c66de4bfd655e0cd293aa5ad8436ca
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-03 19:59:37 -0600

    actividades que me faltaron convertir a c++

---
+++ activ4.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letra;
+    cout << "Ingrese una letra: ";
+    cin >> letra;
+
+    switch(letra) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout << " " << letra << " es una vocal  ";
+            break;
+        default:
+            cout << " " << letra << " no es una vocal  ";
+    }
+
+    return 0;
+}
--- activ4.cpp
+++ activ4.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    char primero, ultimo;
-    cout << "introduzca su primer y ultima inicial: " << endl;
-    cin >>primero >> ultimo;
-    cout << "hola " << primero << "."<< ultimo;
+
+int main (){
+	char letra;
+	cout << "Ingrese una letra:  ";
+	cin >> letra;
+
+	if(letra=='a'|| letra=='e'|| letra=='i'|| letra=='o'|| letra=='u'){
+		cout << letra << " Es una vocal";
+	}else {
+		cout << letra << " No es una vocal";
+	}
     return 0;
-} 
+}
